5d2cf0aa87b82109d36015d8f1d4a09a0cb15ff2
logesh8220 <logeshthirumurugan@gmail.com>
2022-08-13 14:46:59 +0530

    formet

--- src/App.js
+++ src/App.js
@@ -1,40 +1,40 @@
-import logo from './logo.svg';
-import './CSS/sb-admin-2.css';
-import './CSS/sb-admin-2.min.css';
-import Sidebar from './sidebar';
-import Navbar from './Navbar';
-import Dashboard from './Dashbord';
-import Tables from './Tables';
-import Colors from './Colors';
-import Borders from './Borders';
-import React from 'chart.js';
-import Cards from './Cards';
-import Buttons from './Buttons';
-import Login from './Login';
-import {BrowserRouter, Route, Routes} from "react-router-dom";
-import Others from './Others';
+import logo from "./logo.svg";
+import "./CSS/sb-admin-2.css";
+import "./CSS/sb-admin-2.min.css";
+import Sidebar from "./sidebar";
+import Navbar from "./Navbar";
+import Dashboard from "./Dashbord";
+import Tables from "./Tables";
+import Colors from "./Colors";
+import Borders from "./Borders";
+import React from "chart.js";
+import Cards from "./Cards";
+import Buttons from "./Buttons";
+import Login from "./Login";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Others from "./Others";
 function App() {
   return (
-<BrowserRouter>
-<div id='wrapper'>
-      <Sidebar></Sidebar>
-      <div id="content-wrapper" class="d-flex flex-column">
-        <div id="content">
-          <Navbar></Navbar>
-          <Routes>
-            <Route path='/Dashboard' element={<Dashboard/>}/>
-            <Route path='/Tables' element={<Tables/>}/>
-            <Route path='/Colors' element={<Colors/>}/>
-            <Route path='/Cards' element={<Cards/>}/>
-            <Route path='/Buttons' element={<Buttons/>}/>
-            <Route path='/Borders' element={<Borders/>}/>
-            <Route path= '/Others' element={<Others/>}/>
-            <Route path='/Login' element={<Login/>}/>
-          </Routes>
+    <BrowserRouter>
+      <div id="wrapper">
+        <Sidebar></Sidebar>
+        <div id="content-wrapper" class="d-flex flex-column">
+          <div id="content">
+            <Navbar></Navbar>
+            <Routes>
+              <Route path="/Dashboard" element={<Dashboard />} />
+              <Route path="/Tables" element={<Tables />} />
+              <Route path="/Colors" element={<Colors />} />
+              <Route path="/Cards" element={<Cards />} />
+              <Route path="/Buttons" element={<Buttons />} />
+              <Route path="/Borders" element={<Borders />} />
+              <Route path="/Others" element={<Others />} />
+              <Route path="/Login" element={<Login />} />
+            </Routes>
+          </div>
         </div>
       </div>
-    </div>
-</BrowserRouter>
+    </BrowserRouter>
   );
 }
 
--- src/sidebar.jsx
+++ src/sidebar.jsx
@@ -1,94 +1,130 @@
-import React from 'react';
-import {Link}from "react-router-dom"
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faLaughWink ,faTachometerAlt,faTable,faPaintRoller,faClipboard,faTabletButton,faBorderStyle,faFile} from '@fortawesome/free-solid-svg-icons'
-function Sidebar (){
-    return(
-        <div id="wrapper">
-        <ul class="navbar-nav bg-gradient-primary sidebar sidebar-dark accordion" id="accordionSidebar">
-
+import React from "react";
+import { Link } from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faLaughWink,
+  faTachometerAlt,
+  faTable,
+  faPaintRoller,
+  faClipboard,
+  faTabletButton,
+  faBorderStyle,
+  faFile,
+} from "@fortawesome/free-solid-svg-icons";
+function Sidebar() {
+  return (
+    <div id="wrapper">
+      <ul
+        class="navbar-nav bg-gradient-primary sidebar sidebar-dark accordion"
+        id="accordionSidebar"
+      >
         {/* <!-- Sidebar - Brand --> */}
-        <a class="sidebar-brand d-flex align-items-center justify-content-center" href="/Dashboard">
-            <div class="sidebar-brand-icon rotate-n-15">
-                <FontAwesomeIcon class="fas fa-fw " icon={faLaughWink}></FontAwesomeIcon>                                       
-                {/* fa-laugh-wink */}
-            </div>
-            <div class="sidebar-brand-text mx-3">SB Admin <sup>2</sup></div>
+        <a
+          class="sidebar-brand d-flex align-items-center justify-content-center"
+          href="/Dashboard"
+        >
+          <div class="sidebar-brand-icon rotate-n-15">
+            <FontAwesomeIcon
+              class="fas fa-fw "
+              icon={faLaughWink}
+            ></FontAwesomeIcon>
+            {/* fa-laugh-wink */}
+          </div>
+          <div class="sidebar-brand-text mx-3">
+            SB Admin <sup>2</sup>
+          </div>
         </a>
 
         {/* <!-- Divider --> */}
-        <hr class="sidebar-divider my-0"/>
-
+        <hr class="sidebar-divider my-0" />
 
         {/* <!-- Nav Item - Dashboard --> */}
         <li class="nav-item active">
-            <Link class="nav-link" to="/Dashboard">
-                <FontAwesomeIcon class="fas fa-fw mx-2" icon={faTachometerAlt}></FontAwesomeIcon>
-                <span>Dashboard</span></Link>
+          <Link class="nav-link" to="/Dashboard">
+            <FontAwesomeIcon
+              class="fas fa-fw mx-2"
+              icon={faTachometerAlt}
+            ></FontAwesomeIcon>
+            <span>Dashboard</span>
+          </Link>
         </li>
 
         {/* <!-- Divider --> */}
-        <hr class="sidebar-divider"/>
+        <hr class="sidebar-divider" />
         {/* <!-- Heading --> */}
-            <div class="sidebar-heading mx-2">
-                Interface
-            </div>
+        <div class="sidebar-heading mx-2">Interface</div>
 
-                {/* <!-- Nav Item - Dashboard --> */}
-                <li class="nav-item active">
-            <Link class="nav-link" to="/Tables">
-                <FontAwesomeIcon class="fas fa-fw mx-2" icon={faTable}></FontAwesomeIcon>
-                <span>Tables</span></Link>
+        {/* <!-- Nav Item - Dashboard --> */}
+        <li class="nav-item active">
+          <Link class="nav-link" to="/Tables">
+            <FontAwesomeIcon
+              class="fas fa-fw mx-2"
+              icon={faTable}
+            ></FontAwesomeIcon>
+            <span>Tables</span>
+          </Link>
         </li>
 
-
-        <hr class="sidebar-divider"/>
+        <hr class="sidebar-divider" />
         {/* <!-- Heading --> */}
-            <div class="sidebar-heading mx-2">
-                Utilities
-            </div>
+        <div class="sidebar-heading mx-2">Utilities</div>
 
-                {/* <!-- Nav Item - Dashboard --> */}
-                <li class="nav-item active">
-            <Link class="nav-link" to="/Colors">
-                <FontAwesomeIcon class="fas fa-fw mx-2" icon={faPaintRoller}></FontAwesomeIcon>
-                <span>Colors</span></Link>
+        {/* <!-- Nav Item - Dashboard --> */}
+        <li class="nav-item active">
+          <Link class="nav-link" to="/Colors">
+            <FontAwesomeIcon
+              class="fas fa-fw mx-2"
+              icon={faPaintRoller}
+            ></FontAwesomeIcon>
+            <span>Colors</span>
+          </Link>
         </li>
 
-                        {/* <!-- Nav Item - Dashboard --> */}
-                        <li class="nav-item active">
-            <Link class="nav-link" to="/Cards">
-                <FontAwesomeIcon class="fas fa-fw mx-2" icon={faClipboard}></FontAwesomeIcon>
-                <span>Cards</span></Link>
+        {/* <!-- Nav Item - Dashboard --> */}
+        <li class="nav-item active">
+          <Link class="nav-link" to="/Cards">
+            <FontAwesomeIcon
+              class="fas fa-fw mx-2"
+              icon={faClipboard}
+            ></FontAwesomeIcon>
+            <span>Cards</span>
+          </Link>
         </li>
 
-                                {/* <!-- Nav Item - Dashboard --> */}
-                                <li class="nav-item active">
-            <Link class="nav-link" to="/Buttons">
-                <FontAwesomeIcon class="fas fa-fw mx-2" icon={faTabletButton}></FontAwesomeIcon>
-                <span>Buttons</span></Link>
+        {/* <!-- Nav Item - Dashboard --> */}
+        <li class="nav-item active">
+          <Link class="nav-link" to="/Buttons">
+            <FontAwesomeIcon
+              class="fas fa-fw mx-2"
+              icon={faTabletButton}
+            ></FontAwesomeIcon>
+            <span>Buttons</span>
+          </Link>
         </li>
 
-                                        {/* <!-- Nav Item - Dashboard --> */}
-                                        <li class="nav-item active">
-            <Link class="nav-link" to="/Borders">
-                <FontAwesomeIcon class="fas fa-fw mx-2" icon={faBorderStyle}></FontAwesomeIcon>
-                <span>Borders</span></Link>
+        {/* <!-- Nav Item - Dashboard --> */}
+        <li class="nav-item active">
+          <Link class="nav-link" to="/Borders">
+            <FontAwesomeIcon
+              class="fas fa-fw mx-2"
+              icon={faBorderStyle}
+            ></FontAwesomeIcon>
+            <span>Borders</span>
+          </Link>
         </li>
 
-                                                {/* <!-- Nav Item - Dashboard --> */}
-                                                <li class="nav-item active">
-            <Link class="nav-link" to="/Others">
-                <FontAwesomeIcon class="fas fa-fw mx-2" icon={faFile}></FontAwesomeIcon>
-                <span>Others</span></Link>
+        {/* <!-- Nav Item - Dashboard --> */}
+        <li class="nav-item active">
+          <Link class="nav-link" to="/Others">
+            <FontAwesomeIcon
+              class="fas fa-fw mx-2"
+              icon={faFile}
+            ></FontAwesomeIcon>
+            <span>Others</span>
+          </Link>
         </li>
-       
-
-      
-
-    </ul>
+      </ul>
     </div>
-    ) 
-    
+  );
 }
-export default Sidebar
+export default Sidebar;
